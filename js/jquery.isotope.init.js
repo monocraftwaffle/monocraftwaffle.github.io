2b64bbc2cbcae6baeea46c32846d926768883f50
Furkan <furkanbayram2@gmail.com>
2017-11-30 19:24:44 +0200

    first commit

---
+++ js/jquery.isotope.init.js
@@ -0,0 +1,48 @@
+$(document).ready(function($) {
+	"use strict";
+
+	$('.masonry-grid-post').each(function() {
+		var $this = $(this);
+		var $filter = $this.parent().find('.masonry-filter');
+		$this.imagesLoaded(function() {
+			$this.isotope({
+				itemSelector: '.masonry-item'
+			});
+		});
+
+		$filter.find('a').on("click", function(e) {
+			e.preventDefault();
+			$filter.find("a").removeClass('active');
+			$(this).addClass('active');
+			var data_filter = $(this).data('filter');
+			$this.isotope({
+				filter: data_filter
+			});
+		});
+	});
+	
+	$('.isotope-grid-post').each(function() {
+		var $this = $(this);
+		var $filter = $this.parent().find('.masonry-filter');
+		$this.imagesLoaded(function() {
+			$this.isotope({
+				itemSelector: '.isotope-item',
+				percentPosition: true,
+				masonry: {
+					// use element for option
+					columnWidth: '.grid-sizer'
+				}
+			});
+		});
+
+		$filter.find('a').on("click", function(e) {
+			e.preventDefault();
+			$filter.find("a").removeClass('active');
+			$(this).addClass('active');
+			var data_filter = $(this).data('filter');
+			$this.isotope({
+				filter: data_filter
+			});
+		});
+	});
+});
